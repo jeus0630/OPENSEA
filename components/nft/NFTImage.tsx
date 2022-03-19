021075ff801c077d963aa6bff62cf9d6c86c915c
Jewoo <jeus0630@designfever.com>
2022-03-19 19:48:55 +0900

    Add: nft detail page

---
+++ components/nft/NFTImage.tsx
@@ -0,0 +1,33 @@
+import { IoMdSnow } from 'react-icons/io'
+import { AiOutlineHeart } from 'react-icons/ai'
+
+type Props = {
+    selectedNft: {
+        image: string;
+    }
+}
+
+const style = {
+    topBar: `bg-[#303339] p-2 rounded-t-lg border-[#151c22] border`,
+    topBarContent: `flex items-center`,
+    likesCounter: `flex-1 flex items-center justify-end`,
+}
+
+export default function NFTImage({ selectedNft }: Props) {
+    return (
+        <div>
+            <div className={style.topBar}>
+                <div className={style.topBarContent}>
+                    <IoMdSnow />
+                    <div className={style.likesCounter}>
+                        <AiOutlineHeart />
+                        2.3K
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src={selectedNft?.image} />
+            </div>
+        </div>
+    )
+}
